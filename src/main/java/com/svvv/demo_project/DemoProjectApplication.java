78f33ea7f65cb84fb7cf4419fe31f478431c931d
Kartikpatel231 <Kartikpatel1950@gmail.com>
2022-07-01 08:33:56 +0530

    second

---
+++ src/main/java/com/svvv/demo_project/DemoProjectApplication.java
@@ -0,0 +1,11 @@
+package com.svvv.demo_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+public class DemoProjectApplication {
+	public static void main(String[] args) {
+		org.springframework.context.ConfigurableApplicationContext run = SpringApplication.run(DemoProjectApplication.class, args);
+	}
+
+}
